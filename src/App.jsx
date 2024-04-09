7c52d4c384561747a0baf43673c87fe5f1c1d997
ludo06000 <lc06000@gmail.com>
2024-04-09 22:36:27 +0200

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className="h-screen flex justify-center items-center">
+      <div className="text-red-700 font-black text-5xl tracking-widest capitalize flex justify-center md:text-7xl">
+        <p>T</p>
+        <p>E</p>
+        <p>A</p>
+        <p>M</p>
+        <p>&nbsp;</p>
+        <p>D</p>
+        <p>E</p>
+        <p>V</p>
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default App
