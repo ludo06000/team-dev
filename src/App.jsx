1d4ba430655e096cfe0f7551d6a15e5ba2f44330
ludo06000 <lc06000@gmail.com>
2024-04-09 22:50:01 +0200

    update main

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
 
   return (
     <>
-    <div className="h-screen flex justify-center items-center">
+    <div className="h-screen flex flex-col justify-center items-center">
       <div className="text-red-700 font-black text-5xl tracking-widest capitalize flex justify-center md:text-7xl">
         <p>T</p>
         <p>E</p>
@@ -14,6 +14,9 @@ function App() {
         <p>D</p>
         <p>E</p>
         <p>V</p>
+      </div>
+      <div className="text-red-300 font-black text-base tracking-widest flex justify-center md:text-lg">
+        <p>With 💙 by Jerome & Ludo</p>
       </div>
     </div>
     </>
