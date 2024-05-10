6addd7069a51380b0efd75d63ac6cc2530d0c988
ludo06000 <lc06000@gmail.com>
2024-05-10 17:54:55 +0200

    github action

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,15 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-  ],
-  base: "/team-dev" // Remplacez "team" par le nom de votre dépôt GitHub
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: '/',
+  }
+
+  if (command !== 'serve') {
+    config.base = '/team-dev/'
+  }
+
+  return config
 })
